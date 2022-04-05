0543756be688fb7f8fe3582d2cbb01908ca18e26
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-05 16:06:10 +0530

    convert date->str fxn

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ function checkPalindrome(str) {
     }
     return false;
 }
+
+function dateToString(date) {
+    var dateStr = {
+        day : "",
+        month : "",
+        year : "",
+    };
+
+    if (date.day < 10) {
+        dateStr.day = "0" + date.day;
+    }
+    else {
+        dateStr.day = date.day.toString();
+    }
+
+    if (date.month < 10) {
+        dateStr.month = "0" + date.month;
+    }
+    else {
+        dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr;
+}
