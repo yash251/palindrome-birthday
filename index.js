366372f4aaac49336f673ba5e3880743cb35ee15
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-05 15:57:59 +0530

    check palindrome fxn

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function reverseStr(str) {
     return str.split("").reverse().join("");
 }
 
-  
+function checkPalindrome(str) {
+    if (str === stringReverse(str)) {
+      return true;
+    }
+    return false;
+}
