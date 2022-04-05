4316991bddee05fa2d113704d327d6a7b348b86f
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-05 18:28:31 +0530

    get next date fxn

--- index.js
+++ index.js
@@ -49,15 +49,65 @@ function getAllDateFormats(date) {
 }
 
 function checkPalindromeForAllDateFormats(date) {
-    var listOfPalindromes = getAllDateFormats(date);
+    var listOfDateFormats = getAllDateFormats(date);
 
     flag = false;
     
-    for (var i = 0; i < listOfPalindromes.length; i++) {
-        if (checkPalindrome(listOfPalindromes[i])) {
+    for (var i = 0; i < listOfDateFormats.length; i++) {
+        if (checkPalindrome(listOfDateFormats[i])) {
             flag = true;
             break;
         }
     }
     return flag;
+}
+
+function isLeapYear(year) {
+    if (year % 400) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    if (year % 4 === 0) {
+        return true;
+    }
+    return false;
+}
+
+function getNextDate(date) {
+    var day = date.day + 1; // increment the day
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // check for february
+    if (month === 2) {
+        if(isLeapYear(year)) {
+            if (day > 29) {
+                day = 1;
+                month++;
+            }
+        }
+        else {
+            if (day > 28) {
+                day = 1;
+                month++;
+            }
+        }
+    }
+    else {
+        // check if day exceeds the max days in month
+        if (day >daysInMonth[month - 1]) {
+            day = 1;
+            month++;
+        }
+    }
+
+    if (month > 12) {
+        month = 1;
+        year++;
+    }
+
 }
